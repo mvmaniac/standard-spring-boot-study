87390ff4b09b860d4d3fe10de6b490fb03cc413c
yhs <mvmaniaz@gmail.com>
2020-09-08 23:54:58 +0900

    [추가] 모임 진행 중

--- src/main/resources/static/js/common-study.js
+++ src/main/resources/static/js/common-study.js
@@ -1,3 +1,26 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+
+  moment.locale('ko');
+  $(".date-time").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").format('LLL');
+  });
+  $(".date").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").format('LL');
+  });
+  $(".weekday").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").format('dddd');
+  });
+  $(".time").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").format('LT');
+  });
+  $(".calendar").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").calendar();
+  });
+  $(".fromNow").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").fromNow();
+  });
+  $(".date-weekday-time").text(function(index, dateTime) {
+    return moment(dateTime, "YYYY-MM-DD`T`hh:mm").format('LLLL');
+  });
 });
