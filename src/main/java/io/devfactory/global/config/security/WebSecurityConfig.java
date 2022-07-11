b47d5d0db18af826a73edd151f8985bf69df96a4
yhs <mvmaniaz@gmail.com>
2022-07-11 23:38:33 +0900

    Refactor: WebSecurityConfigurerAdapter deprecated

--- src/main/java/io/devfactory/global/config/security/WebSecurityConfig.java
+++ src/main/java/io/devfactory/global/config/security/WebSecurityConfig.java
@@ -1,24 +1,28 @@
 package io.devfactory.global.config.security;
 
 import io.devfactory.global.config.security.service.UserAccountService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.RequestCacheConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
   private final UserAccountService userAccountService;
   private final DataSource dataSource;
@@ -26,48 +30,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Value("${spring.profiles.active:default}")
   private String activeProfile;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Order(0)
+  @Bean
+  public SecurityFilterChain resourceChain(HttpSecurity http) throws Exception {
     // @formatter:off
-    http
-      .authorizeRequests()
+    return http
+      .requestMatchers(matchers -> matchers.antMatchers(StaticResource.getResources("/node_modules/**")))
+      .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+      .requestCache(RequestCacheConfigurer::disable)
+      .securityContext(AbstractHttpConfigurer::disable)
+      .sessionManagement(AbstractHttpConfigurer::disable)
+      .build();
+    // @formatter:on
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // @formatter:off
+    final var httpSecurity = http
+      .authorizeRequests(authorize -> authorize
         .antMatchers("/h2-console/**", "/", "/sign-up", "/check-email-token", "/email-login", "/login-by-email", "/search/study")
           .permitAll()
         .antMatchers(HttpMethod.GET, "/profile/*")
           .permitAll()
         .anyRequest()
-          .authenticated()
-        .and()
+          .authenticated())
+      .formLogin(form -> form.loginPage("/login").permitAll())
+      .logout(logout -> logout.logoutSuccessUrl("/"))
+      .rememberMe(remember -> remember.userDetailsService(userAccountService).tokenRepository(tokenRepository()));
 
-      .formLogin()
-        .loginPage("/login")
-          .permitAll()
-        .and()
-
-      .logout()
-        .logoutSuccessUrl("/")
-        .and()
-
-      .rememberMe()
-        .userDetailsService(userAccountService)
-        .tokenRepository(tokenRepository())
-    // @formatter:on
-    ;
-
-    // 개발 환경용 설정
     if (List.of("default", "local").contains(activeProfile)) {
       // @formatter:off
-      http
-        .headers()
-          .frameOptions()
-          .sameOrigin()
-
-        .and()
-          .csrf()
-            .ignoringAntMatchers("/h2-console/**")
-        ;
+      httpSecurity
+        .headers(header -> header.frameOptions().sameOrigin())
+        .csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"));
       // @formatter:on
     }
+
+    return httpSecurity.build();
+    // @formatter:on
   }
 
   @Bean
@@ -77,11 +78,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     return jdbcTokenRepository;
   }
 
-  @Override
-  public void configure(WebSecurity web) {
-    web.ignoring()
-        .antMatchers("/node_modules/**")
-        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+  @Getter
+  public static class StaticResource {
+
+    private StaticResource() {
+      throw new IllegalStateException("Constructor not supported");
+    }
+
+    private static final String[] defaultResources = Arrays.stream(StaticResourceLocation.values())
+        .flatMap(StaticResourceLocation::getPatterns)
+        .toArray(String[]::new);
+
+    public static String[] getResources() {
+      return defaultResources;
+    }
+
+    public static String[] getResources(String... antPatterns) {
+      final var defaultResources = StaticResource.defaultResources;
+      final var resources = Arrays.copyOf(defaultResources, defaultResources.length + antPatterns.length);
+      System.arraycopy(antPatterns, 0, resources, defaultResources.length, antPatterns.length);
+      return resources;
+    }
+
   }
 
 }
